647b267218c2eba903c3278a1c3868db1e3f0ee6
shehabSh <shehab.e.sh@gmail.com>
2023-03-09 01:03:42 +0200

    new update

--- eis/settings.py
+++ eis/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
+    'django.contrib.humanize',
+    
 ]
 
 MIDDLEWARE = [
@@ -114,10 +117,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ 
+    BASE_DIR / "static",
+    ]
+#MEDIA
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login'
