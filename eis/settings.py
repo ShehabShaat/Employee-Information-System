0c863b2dbdef9db15f774c689b0d6b86898da3d0
shehabSh <shehab.e.sh@gmail.com>
2023-03-09 18:12:01 +0200

    new update

--- eis/settings.py
+++ eis/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'app',
     'django.contrib.humanize',
     'rest_framework',
-    'corsheaders',
+    'corsheaders'
     
 ]
 
@@ -138,7 +138,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [ 
+    BASE_DIR / "static",
+    ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 #MEDIA
 MEDIA_URL = 'media/'
