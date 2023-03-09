63fad8262c718262124447e566b265603089d77f
shehabSh <shehab.e.sh@gmail.com>
2023-03-09 17:56:26 +0200

    new update

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@
 from django.urls import include, path
 from . import views
 from .views import *
-from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
 	path('', views.apiOverview, name="api-overview"),
