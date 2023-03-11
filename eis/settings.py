b79c3755a3610be6318da26ca682a8e90d6deee7
shehabSh <shehab.e.sh@gmail.com>
2023-03-11 19:54:54 +0200

    new update

--- eis/settings.py
+++ eis/settings.py
@@ -158,10 +158,10 @@ LOGIN_URL = '/login'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
-    ],
-    #  'DEFAULT_AUTHENTICATION_CLASSES': [
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated'
+    # ],
+    # #  'DEFAULT_AUTHENTICATION_CLASSES': [
     #     'rest_framework.authentication.TokenAuthentication'
     # ]
     
